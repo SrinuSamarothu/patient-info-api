a2ac91fb59bccaa2df5ecbb601e948bde7dba7ec
manojkmj11 <mithuna692@gmail.com>
2023-04-14 16:54:01 +0530

    added CORS

--- PatientinfoApi/Program.cs
+++ PatientinfoApi/Program.cs
@@ -14,6 +14,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ILogic, Logic>();
 builder.Services.AddScoped<IRepo , Repo>();
 
+var AllowAllPolicy = "AllowAllPolicy";
+builder.Services.AddCors(options =>
+    options.AddPolicy(AllowAllPolicy, policy => { policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); }));
+
 /*
 var config = builder.Configuration.GetConnectionString("connectionstring");*/
 builder.Services.AddDbContext<PatientInfoServiceDbContext>(options => options.UseSqlServer("Server=tcp:teamperseverance.database.windows.net,1433;Initial Catalog=patientinfodb;Persist Security Info=False;User ID=manoj;Password=Team@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
@@ -25,6 +29,7 @@ var app = builder.Build();
 //     app.UseSwagger();
 //     app.UseSwaggerUI();
 // }
+app.UseCors(AllowAllPolicy);
 
 app.UseHttpsRedirection();
 
